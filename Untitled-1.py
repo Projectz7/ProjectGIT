6abf438cd75c64f0b0495593574d345359d35413
Projectz7 <118845898+Projectz7@users.noreply.github.com>
2022-11-26 16:32:50 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+print('Programa de integração')
+a=10
+b=20
+c=30
+soma = a + b + c + a
+quadrado = soma**2
+print(soma)
+print(quadrado)
+print(soma==100)
+
+if soma==70:
+    print ('acertou')
+else:
+    print ('Errou')
+
+
+
